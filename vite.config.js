ba96e093f2ee96aca258d7cb92d404154e7057ca
AbeliaChen <aurorachen0926@163.com>
2026-02-15 15:02:33 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/maolin-portfolio-cn/',   // 👈 关键：改成你的仓库名
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
